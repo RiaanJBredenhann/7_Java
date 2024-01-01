ec39ddac4bd9a4a36bfe54823b98fdcd857680bc
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-01 13:57:17 +0200

    final

---
+++ Chapter6/SoccerLeague/src/soccer/Goal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer;
+
+/**
+ *
+ * @author Riaan HP
+ */
+public class Goal {
+    
+    public Team theTeam;
+    public Player thePlayer;
+    public double theTime;
+    
+}
