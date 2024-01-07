b9dfa979e02da02c84687e7b69bd8a98f17ad67f
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-07 16:40:55 +0200

    update

---
+++ Chapter14/Chapter14Lesson1/src/exercise/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise;
+
+public class Calculator {
+    public int add(int x, int y){
+        return x + y;
+    }
+    
+    // This method could throw an ArithmeticException
+    public double divide(int x, int y) throws ArithmeticException {
+        return x / y;
+    }
+}
+
