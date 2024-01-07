a1ed48340c71b04ed64bb5bfd3dcd17228cfd775
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-07 14:59:37 +0200

    update

---
+++ Chapter9/Chapter9Lesson1/src/exercise/ShoppingCart.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise;
+
+/**
+ *
+ * @author Riaan HP
+ */
+public class ShoppingCart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // Declare, instantiate, and initialize a Customer object
+        Customer cust1 = new Customer("Bob Miller", "555-44-3212");
+
+	// Print the customer object name
+        System.out.println("Customer name: "+cust1.getName());
+    }
+    
+}
