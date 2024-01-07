a2cb93b22c0f5add7d0908a036a58cac6e4960fd
RiaanJBredenhann <riaanbredenhann@gmail.com>
2024-01-07 16:34:57 +0200

    update

---
+++ Chapter13/Chapter13Lesson2/src/exercise/ShoppingCart.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class ShoppingCart {
+    ArrayList<Item> items = new ArrayList();
+    
+    public static void main(String[] args){
+        ShoppingCart cart = new ShoppingCart();
+        cart.fillCart();
+        cart.removeItemFromCart("Trousers");
+    }
+    
+    public void fillCart(){
+        items.add(new Shirt(40.95,'M','R'));
+        items.add(new Shirt(32.99,'M','Y'));
+        items.add(new Trousers(59.99,34,'B',"Relaxed",'M'));
+        items.add(new Trousers(75.50,8,'G',"Skinny",'F'));
+    }
+    
+    public void removeItemFromCart(String desc){
+        // remove all Trousers from the items list, then print out the list
+        items.removeIf(i -> i.getDesc().equals(desc));
+        System.out.println("Cart after removing Trousers: \n"+items);
+    }
+    
+}
